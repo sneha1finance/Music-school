6dc1f92692063fbee4ff414dfcf5014c96a9703e
sneha1finance <sneha.yadav@1finance.co.in>
2024-04-18 00:55:46 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images:{
+        domains:['images.unsplash.com','res.cloudinary.com']
+    }
+};
+//to configure images of unsplash we need to add domains in this files
 export default nextConfig;
